5fc62be3f8123c2b8b3238ee768796a834181e30
farzin-f <farzin.faridfar@gmail.com>
2017-03-01 07:05:04 -0500

    skimage canny and regionprops - some properties approximations

---
+++ cell_migration.py
@@ -0,0 +1,45 @@
+import os
+from skimage.util import img_as_bool
+from skimage import io
+import matplotlib.pyplot as plt
+
+import numpy as np
+from skimage.transform import (hough_line, hough_line_peaks,
+                               probabilistic_hough_line)
+from skimage.feature import canny
+from skimage import data
+
+from skimage.measure import label, regionprops
+import math
+
+
+image = img_as_bool(io.imread("cell.tif", as_grey=True)+0.15)
+
+edges = canny(image)
+
+label_img = label(edges)
+regions = regionprops(label_img)
+
+fig, ax = plt.subplots()
+ax.imshow(edges, cmap=plt.cm.gray)
+
+for props in regions:
+    y0, x0 = props.centroid
+    orientation = props.orientation
+    x1 = x0 + math.cos(orientation) * 0.5 * props.major_axis_length
+    x0 = x0 - math.cos(orientation) * 0.5 * props.major_axis_length
+    y1 = y0 - math.sin(orientation) * 0.5 * props.major_axis_length
+    y0 = y0 + math.sin(orientation) * 0.5 * props.major_axis_length
+
+
+    # reference: http://scikit-image.org/docs/dev/api/skimage.measure.html#skimage.measure.regionprops
+    print "Length: ", props.major_axis_length
+    print "Orientation [-pi/2, pi/2]: ", orientation
+    print "Area (pixs): ", props.area
+    
+    ax.plot((x0, x1), (y0, y1), '-r', linewidth=1.5)
+
+
+fig.tight_layout()
+plt.show()
+
